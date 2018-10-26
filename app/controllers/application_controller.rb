c45d1b990d2a4d34a229f563c35d81d3e01be71b
Yomaira Escano <yjescano@gmail.com>
2018-10-26 02:17:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
     end
 
     def logged_in?
-      !!session[:token]
+      !!@access_token = access_hash["access_token"]
+      # !!session[:token]
     end
 end
