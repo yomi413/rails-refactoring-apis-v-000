bc4d75d1fc4cf2b5f2c3472c1f03fb9d802dc8af
Yomaira Escano <yjescano@gmail.com>
2018-10-26 02:21:19 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     github = GithubService.new
     @access_token = github.authenticate!(ENV["GITHUB_CLIENT"], ENV["GITHUB_SECRET"], params[:code])
-    username = github.get_username
+    @username = github.get_username
     redirect_to '/'
     # response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
     # access_hash = JSON.parse(response.body)
